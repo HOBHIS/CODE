27df09f9076507876713e69d8e532a42e9172469
hobhis <HOBHISUPMC@outlook.com>
2015-02-08 18:06:11 +0100

    hello,我又更新了，这次使用exlipes

--- bgw/src/bgw.cpp
+++ bgw/src/bgw.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : bgw.cpp
-// Author      :
-// Version     :
+// Author      :Bgw
+// Version     :V0.1
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!! Je t'aime!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!! Je t'aime!! C‘est quoi ca?" << endl; // prints !!!Hello World!!!
 	return 0;
 }
