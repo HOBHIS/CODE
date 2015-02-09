d91d4c6e96ed24cdb819ac63c1c0df1ee871b053
hobhis <HOBHISUPMC@outlook.com>
2015-02-09 21:12:59 +0100

    hi我改了东哥的东西

--- bgw/src/bgw.cpp
+++ bgw/src/bgw.cpp
@@ -13,6 +13,6 @@ int main() {
 	cout << "!!!Hello World!!! Je t'aime!! C‘est quoi ca?" << endl; // prints !!!Hello World!!!
 	return 0;
 }
- //你这是写的啥阿，在java里用c++ 的代码
+ //你这是写的啥阿.............
 
 //我好像成功了埃。。。。
